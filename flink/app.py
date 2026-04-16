55932c8031f7bc97c2a026e5121fac8a2da90d50
edu-kenshi <edu.kenshi@gmail.com>
2026-04-16 15:00:19 +0900

    Feat: 원본 데이터를 받아서 특정 컬럼만 테이블에 저장하도록 테이블 구성

--- flink/app.py
+++ flink/app.py
@@ -17,14 +17,46 @@ def main():
     setting = EnvironmentSettings.new_instance().in_streaming_mode().build()
     # SQL과 유사한 방식으로 데이터를 다룰수 있는 객체
     t_env = TableEnvironment( setting )
+    
+    '''
+        로그 원문 1개
+        {   
+            'event_time': '2026-04-16T11:22:48.570824', 
+            'ticker': 'NVDA', 
+            'price': 248.62, 
+            'volume': 34, 
+            'trade_id': 1246420
+        }
+    '''
 
     # 2. 입력데이터에 대한 테이블 구성 (kds로부터(INPUT) 데이터를 읽기 처리->어딘가에 담는다->테이블)
     #    티커, 가격, 로그발생시간, ..
+    #    입력데이터에 대한 테이블에 kds가 연결되어 있어야함
+    t_env.execute_sql('''
+        create table stock_input (
+            ticker STRING,
+            price DOUBLE,
+            event_time TIMESTAMP(3),
+            WATERMARK FOR event_time AS event_time - INTERVAL '5' SECOND
+        ) with (
+            "connector" = "kinesis",
+            "stream"    = "de-ai-30-an2-kds-stock-input",
+            "aws.region"= "ap-northeast-2",
+            "scan.stream.initpos" = "LATEST",
+            "format"    = "json"
+        )
+    ''')
     
     # 3. 출력데이터에 대한 테이블 구성 (kds로부터(output) 데이터를 읽기 처리->어딘가에 담는다->테이블)
     #    티커, 평균가격, 생성시간
+    #    출력데이터에 대한 테이블에 kds가 연결되어 있어야함
+    t_env.execute_sql('''
+        create table stock_input () with ()
+    ''')
 
     # 4. 연산(전처리, 가공, 분석(요구사항에 맞게)처리한 형태) 및 전송(kds(OUTUT)  전송)
+    t_env.execute_sql('''
+    ''')
     pass
 
 # 단독형 앱 => 엔트리 포인트 표기 필요!!
